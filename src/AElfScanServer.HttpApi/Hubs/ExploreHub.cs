9a6be1df09581ab891865801fd31567963507807
alanexplore <alanexplore@aelfscan.io>
2024-11-20 12:45:05 +0800

    update

--- src/AElfScanServer.HttpApi/Hubs/ExploreHub.cs
+++ src/AElfScanServer.HttpApi/Hubs/ExploreHub.cs
@@ -140,7 +140,7 @@ public class ExploreHub : AbpHub
     {
         await RequestMergeChainInfo();
 
-        PushMergeBlockInfoAsync(request.ChainId);
+        PushMergeBlockInfoAsync();
     }
 
 
