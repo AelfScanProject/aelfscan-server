b6a4dfb6ed91281eafbc8997160012c486765111
alanexplore <alanexplore@aelfscan.io>
2024-11-20 10:13:33 +0800

    update test

--- test/AElfScanServer.Worker.Tests/AElfScanServerWorkerTestModule.cs
+++ test/AElfScanServer.Worker.Tests/AElfScanServerWorkerTestModule.cs
@@ -13,11 +13,6 @@ public class AElfScanServerWorkerTestModule : AbpModule
     public override void ConfigureServices(ServiceConfigurationContext context)
     {
         context.Services.AddLogging(configure => configure.AddConsole());
-        context.Services.AddSingleton<ChainTestHelper>();
-        context.Services.AddSingleton<TradePairTestHelper>();
-        context.Services.AddSingleton(sp => sp.GetService<ClusterFixture>().Cluster.Client);
-        context.Services.AddSingleton<IAElfClientProvider, MockAelfClientProvider>();
-        context.Services.AddSingleton<ISyncStateProvider, MockSyncStateProvider>();
-        context.Services.Configure<PortfolioOptions>(o => { o.DataVersion = "v1"; });
+        
     }
 }
