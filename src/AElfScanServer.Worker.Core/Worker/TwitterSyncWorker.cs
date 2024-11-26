24d2c71b772a97a69fa5e44b0fe141e9125985e4
EchoForge7 <EchoForge7@aelfscan.io>
2024-11-26 11:52:25 +0800

    feat:update twitter sync config

--- src/AElfScanServer.Worker.Core/Worker/TwitterSyncWorker.cs
+++ src/AElfScanServer.Worker.Core/Worker/TwitterSyncWorker.cs
@@ -1,7 +1,9 @@
 using System.Threading.Tasks;
 using AElfScanServer.HttpApi.Service;
+using AElfScanServer.Worker.Core.Options;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Volo.Abp.BackgroundWorkers;
 using Volo.Abp.Threading;
 
@@ -13,13 +15,15 @@ public class TwitterSyncWorker : AsyncPeriodicBackgroundWorkerBase
     private readonly IAdsService _adsService;
 
     private readonly ILogger<TwitterSyncWorker> _logger;
+    private const string WorkerName = "TwitterSyncWorker";
+
 
 
     public TwitterSyncWorker(AbpAsyncTimer timer, IServiceScopeFactory serviceScopeFactory,
-        ILogger<TwitterSyncWorker> logger, IAdsService adsService) : base(timer,
+        ILogger<TwitterSyncWorker> logger, IAdsService adsService, IOptionsMonitor<WorkerOptions> workerOptions) : base(timer,
         serviceScopeFactory)
     {
-        timer.Period = 1000 * 60 * 60;
+        timer.Period = workerOptions.CurrentValue.GetWorkerPeriodMinutes(WorkerName) * 60 * 1000;
         timer.RunOnStart = true;
         _logger = logger;
         _adsService = adsService;
