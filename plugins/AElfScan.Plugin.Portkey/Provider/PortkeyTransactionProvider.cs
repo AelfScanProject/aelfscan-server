637d80c61d00b9b3988dbea47b8a21aecec90d70
EchoForge7 <EchoForge7@aelfscan.io>
2024-12-27 17:48:34 +0800

    feat:update portkey

--- plugins/AElfScan.Plugin.Portkey/Provider/PortkeyTransactionProvider.cs
+++ plugins/AElfScan.Plugin.Portkey/Provider/PortkeyTransactionProvider.cs
@@ -1,14 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AElf.ExceptionHandler;
-using AElf.Indexing.Elasticsearch;
-using AElfScanServer.Common.ExceptionHandling;
+using System.Diagnostics.Metrics;
+using AElf.OpenTelemetry;
 using AElfScanServer.Common.IndexerPluginProvider;
 using CAServer.CAActivity.Provider;
 using GraphQL;
-using Nest;
+using Microsoft.Extensions.Logging;
 using Portkey.UserAssets;
 using Volo.Abp.DependencyInjection;
 
@@ -17,38 +12,50 @@ namespace Portkey.Provider;
 public class PortkeyTransactionProvider : IPortkeyTransactionProvider, ISingletonDependency, IAddressTypeProvider
 {
     private readonly IGraphQLHelper _graphQlHelper;
-
-
-    public PortkeyTransactionProvider(IGraphQLHelper graphQlHelper
+    private static Counter<long> _alarmCounter;
+    private readonly ILogger<GraphQLHelper> _logger;
+    public PortkeyTransactionProvider(IGraphQLHelper graphQlHelper,IInstrumentationProvider instrumentationProvider
     )
     {
         _graphQlHelper = graphQlHelper;
+        _alarmCounter = instrumentationProvider.Meter.
+            CreateCounter<long>(
+                "aelfScanAlarmCount", 
+                "counts", 
+                "The number of Alarm"
+            );
     }
 
 
-    [ExceptionHandler( typeof(Exception),
-        Message = "GetActivitiesAsync err",
-        TargetType = typeof(ExceptionHandlingService),
-        MethodName = nameof(ExceptionHandlingService.AlarmException),LogTargets = ["Portkey"])]
     public async Task<IndexerTransactions> GetActivitiesAsync(List<CAAddressInfo> caAddressInfos, string inputChainId,
         string symbolOpt, List<string> inputTransactionTypes, int inputSkipCount, int inputMaxResultCount,string transactionId = "")
     {
-        return await _graphQlHelper.QueryAsync<IndexerTransactions>(new GraphQLRequest
+        try
         {
-            Query = @"
+            return   await _graphQlHelper.QueryAsync<IndexerTransactions>(new GraphQLRequest
+            {
+                Query = @"
 			    query ($chainId:String,$transactionId:String,$symbol:String,$caAddressInfos:[CAAddressInfo]!,$methodNames:[String],$startBlockHeight:Long!,$endBlockHeight:Long!,$skipCount:Int!,$maxResultCount:Int!) {
                     caHolderTransaction(dto: {chainId:$chainId,transactionId:$transactionId,symbol:$symbol,caAddressInfos:$caAddressInfos,methodNames:$methodNames,startBlockHeight:$startBlockHeight,endBlockHeight:$endBlockHeight,skipCount:$skipCount,maxResultCount:$maxResultCount}){
                         data{id,chainId,blockHash,blockHeight,previousBlockHash,transactionId,methodName,tokenInfo{symbol,tokenContractAddress,decimals,totalSupply,tokenName},status,timestamp,nftInfo{symbol,totalSupply,imageUrl,decimals,tokenName},transferInfo{fromAddress,toAddress,amount,toChainId,fromChainId,fromCAAddress},fromAddress,transactionFees{symbol,amount},isManagerConsumer,
                             toContractAddress,tokenTransferInfos{tokenInfo{symbol,decimals,tokenName,tokenContractAddress},nftInfo{symbol,decimals,tokenName,collectionName,collectionSymbol,inscriptionName,imageUrl},transferInfo{amount,fromAddress,fromCAAddress,toAddress,fromChainId,toChainId}}},totalRecordCount
                     }
                 }",
-            Variables = new
-            {
-                caAddressInfos = caAddressInfos, chainId = inputChainId, symbol = symbolOpt,
-                methodNames = inputTransactionTypes, skipCount = inputSkipCount, maxResultCount = inputMaxResultCount,
-                startBlockHeight = 0, endBlockHeight = 0,transactionId=transactionId
-            }
-        });
+                Variables = new
+                {
+                    caAddressInfos = caAddressInfos, chainId = inputChainId, symbol = symbolOpt,
+                    methodNames = inputTransactionTypes, skipCount = inputSkipCount, maxResultCount = inputMaxResultCount,
+                    startBlockHeight = 0, endBlockHeight = 0,transactionId=transactionId
+                }
+            });
+        }
+        catch (Exception e)
+        {
+            _alarmCounter.Add(1,new KeyValuePair<string, object>("alarmType","Portkey"));
+            _logger.LogError(e,"GetActivitiesAsync error");
+        }
+
+        return new IndexerTransactions();
     }
 
     public async Task<string> GetAddressType(string chainId, string address)
@@ -63,23 +70,28 @@ public class PortkeyTransactionProvider : IPortkeyTransactionProvider, ISingleto
         return "";
     }
 
-    [ExceptionHandler( typeof(Exception),
-        Message = "GetCaHolderManagerInfoAsync err",
-        TargetType = typeof(ExceptionHandlingService),
-        MethodName = nameof(ExceptionHandlingService.AlarmException),LogTargets = ["Portkey"])]
     public async Task<CAHolderInfo> GetCaHolderManagerInfoAsync(List<string> caAddresses)
     {
-        return await _graphQlHelper.QueryAsync<CAHolderInfo>(new GraphQLRequest
+        try
         {
-            Query = @"
+            await _graphQlHelper.QueryAsync<CAHolderInfo>(new GraphQLRequest
+            {
+                Query = @"
 			    query($caAddresses:[String],$skipCount:Int!,$maxResultCount:Int!) {
                     caHolderManagerInfo(dto: {caAddresses:$caAddresses,skipCount:$skipCount,maxResultCount:$maxResultCount}){
                         originChainId,chainId,caHash,caAddress, managerInfos{address,extraData}}
                 }",
-            Variables = new
-            {
-                caAddresses, skipCount = 0, maxResultCount = caAddresses.Count
-            }
-        });
+                Variables = new
+                {
+                    caAddresses, skipCount = 0, maxResultCount = caAddresses.Count
+                }
+            });
+        }
+        catch (Exception e)
+        {
+            _alarmCounter.Add(1,new KeyValuePair<string, object>("alarmType","Portkey"));
+            _logger.LogError(e,"GetCaHolderManagerInfoAsync error");
+        }
+        return new CAHolderInfo();
     }
 }
